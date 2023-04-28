77112013d164d2773ca5587ffcdee690d3603493
SolitudeFate <2635119206@qq.com>
2023-04-28 17:25:44 +0800

    封装了在系统相册选择图片的代码

--- app/src/main/java/com/example/baiduai/FaceDetection.java
+++ app/src/main/java/com/example/baiduai/FaceDetection.java
@@ -1,73 +1,42 @@
 package com.example.baiduai;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import com.example.baiduai.utils.ImagePickerUtils;
 import com.example.baiduai.utils.PermissionUtils;
 import com.example.baiduai.utils.ToolbarUtils;
-import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.android.material.snackbar.Snackbar;
 
 public class FaceDetection extends AppCompatActivity implements View.OnClickListener {
 
     private ToolbarUtils mToolbarUtils;
     private ImageView face_dect_img1;
-    private ActivityResultLauncher<Intent> resultLauncher;
-    private ImageView top_app_bar_image;
-    private MaterialToolbar top_app_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.face_detection);
+
         // 权限申请
         PermissionUtils.checkPermissions(this);
 
-        // 添加图片
-        face_dect_img1 = findViewById(R.id.face_dect_img1);
-        face_dect_img1.setOnClickListener(this);
-
-        // topAppBar
-//        top_app_bar_image = findViewById(R.id.topAppBarImage);
-//        top_app_bar = findViewById(R.id.topAppBar);
-//        top_app_bar.setOnMenuItemClickListener(this);
-//        top_app_bar_image.setOnClickListener(this);
+        // 加载topAppBar
         mToolbarUtils = new ToolbarUtils(this);
 
-        // 跳转到系统相册，选择图片并返回
-        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == RESULT_OK) {
-                    Intent intent = result.getData();
-                    //获取选中图片的路径对象
-                    Uri picUri = intent.getData();
-                    if (picUri != null) {
-                        face_dect_img1.setImageURI(picUri);
-                        Log.d("lyk", "picUri:" + picUri.toString());
-                    }
-                }
-            }
-        });
+        // 选择图片
+        face_dect_img1 = findViewById(R.id.face_dect_img1);
+        face_dect_img1.setOnClickListener(this);
     }
 
+    // 动态申请权限
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
         PermissionUtils.handlePermissionsResult(this, requestCode, permissions, grantResults);
     }
 
@@ -75,45 +44,18 @@ public class FaceDetection extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.face_dect_img1:
-                // 跳转到系统相册，选择图片，并返回
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                // 设置内容类型为图片类型
-                intent.setType("image/*");
-                // 打开系统相册，并等待图片选择结果
-                resultLauncher.launch(intent);
+                // 选择图片
+                ImagePickerUtils.pickImage(this, face_dect_img1);
                 break;
-//            case R.id.topAppBarImage:
-//                Snackbar.make(v, R.string.snackbar_text_label, Snackbar.LENGTH_SHORT).show();
-//                break;
         }
     }
 
-//    @Override
-//    public boolean onMenuItemClick(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.tips:
-//                new MaterialAlertDialogBuilder(this)
-//                        .setTitle(this.getResources().getString(R.string.tips_title))
-//                        .setMessage(this.getResources().getString(R.string.tips_supporting_text))
-//                        .setPositiveButton(this.getResources().getString(R.string.btn_close), (dialog, which) -> {
-//                        })
-//                        .show();
-//                return true;
-//            case R.id.favorite:
-//                MaterialAlertDialogBuilder favoriteDialogBuilder = new MaterialAlertDialogBuilder(this)
-//                        .setTitle(this.getResources().getString(R.string.favorite_title))
-//                        .setMessage(this.getResources().getString(R.string.favorite_supporting_text));
-//
-//                ImageView imageView = new ImageView(this);
-//                imageView.setImageResource(R.drawable.github);
-//                favoriteDialogBuilder.setView(imageView);
-//
-//                favoriteDialogBuilder.setPositiveButton(this.getResources().getString(R.string.btn_close), (dialog, which) -> {
-//                        })
-//                        .show();
-//                return true;
-//            default:
-//                return false;
-//        }
-//    }
+    // 处理选择的图片的base64
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String base64Image = ImagePickerUtils.handleActivityResult(this, requestCode, resultCode, data, face_dect_img1);
+        if (base64Image != null) {
+            Log.d("lyk", base64Image);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/baiduai/utils/ImagePickerUtils.java
@@ -0,0 +1,54 @@
+package com.example.baiduai.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ImagePickerUtils {
+    private static final int REQUEST_CODE_PICK_IMAGE = 1;
+
+    // 打开系统相册，选择一张图片，并将其显示在传入的ImageView中
+    public static void pickImage(Activity activity, ImageView imageView) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        activity.startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+        setImageView(imageView, null);
+    }
+
+    // 将选择的图片转换为Base64，返回值为base64。如果选择的图片无法转换或未选择图片，则返回null。
+    public static String handleActivityResult(Activity activity, int requestCode, int resultCode, Intent data, ImageView imageView) {
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            if (selectedImage != null) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), selectedImage);
+                    setImageView(imageView, bitmap);
+                    return convertBitmapToBase64(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private static void setImageView(ImageView imageView, Bitmap bitmap) {
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private static String convertBitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+}
