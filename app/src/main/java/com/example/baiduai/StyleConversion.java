069d824e0fe8713d90142acacb898675dc908c78
SolitudeFate <2635119206@qq.com>
2023-04-29 16:56:47 +0800

    设计了单选按钮的样式

---
+++ app/src/main/java/com/example/baiduai/StyleConversion.java
@@ -0,0 +1,74 @@
+package com.example.baiduai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chaquo.python.Kwarg;
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.example.baiduai.utils.ImagePickerUtils;
+import com.example.baiduai.utils.ImageUtils;
+import com.example.baiduai.utils.ToolbarUtils;
+
+public class StyleConversion extends AppCompatActivity implements View.OnClickListener {
+    private ToolbarUtils mToolbarUtils;
+    private ImageView img1;
+    private String base64Image = null;
+    private ImageView img2;
+    private Python py;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.style_conversion);
+
+        // 加载topAppBar
+        mToolbarUtils = new ToolbarUtils(this);
+
+        // 选择图片
+        img1 = findViewById(R.id.img1);
+        img1.setOnClickListener(this);
+
+        // 显示图片
+        img2 = findViewById(R.id.img2);
+
+        findViewById(R.id.btn_main).setOnClickListener(this);
+
+        initPython();
+        py = Python.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.img1:
+                // 选择图片
+                ImagePickerUtils.pickImage(this, img1);
+                break;
+
+            case R.id.btn_main:
+                PyObject obj1 = py.getModule("style_conversion")
+                        .callAttr("main", new Kwarg("img_base64", base64Image));
+                base64Image = obj1.toJava(String.class);
+                ImageUtils.saveBase64ImageToGallery(v,img2,this, base64Image);
+                break;
+        }
+    }
+
+    // 将选择的图片的转成base64
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        base64Image = ImagePickerUtils.handleActivityResult(this, requestCode, resultCode, data, img1);
+    }
+
+    void initPython() {
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+    }
+}
